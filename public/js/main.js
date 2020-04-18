137316c3eb3462d341d4db3248ca0dbdbd12a4ab
Sean Stoneburner <seanstoneburner@Seans-MacBook-Pro.local>
2020-04-18 07:11:37 -0400

    added backgournd to level.json and loaded into main

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,15 @@
 import SpriteSheet from "./SpriteSheet.js";
-import { loadImage } from "./loaders.js";
+import { loadImage, loadLevel } from "./loaders.js";
+
+function drawBackground(background, context, sprites) {
+  background.ranges.forEach(([x1, x2, y1, y2]) => {
+    for (let x = x1; x < x2; ++x) {
+      for (let y = y1; y < y2; ++y) {
+        sprites.drawTile(background.tile, context, x, y);
+      }
+    }
+  });
+}
 
 const canvas = document.getElementById("screen");
 const context = canvas.getContext("2d");
@@ -10,21 +20,21 @@ loadImage("/images/tiles.png").then((image) => {
   sprites.define("grass", 0, 0);
   sprites.define("ground", 0, 1);
 
-  for (let x = 0; x < 25; ++x) {
-    for (let y = 0; y < 14; ++y) {
-      sprites.drawTile("sky", context, x, y);
-    }
-  }
+  loadLevel("1-1").then((level) => {
+    level.backgrounds.forEach((background) =>
+      drawBackground(background, context, sprites)
+    );
+  });
 
-  for (let x = 0; x < 25; ++x) {
-    for (let y = 12; y < 13; ++y) {
-      sprites.drawTile("grass", context, x, y);
-    }
-  }
+  // for (let x = 0; x < 25; ++x) {
+  //   for (let y = 12; y < 13; ++y) {
+  //     sprites.drawTile("grass", context, x, y);
+  //   }
+  // }
 
-  for (let x = 0; x < 25; ++x) {
-    for (let y = 13; y < 14; ++y) {
-      sprites.drawTile("ground", context, x, y);
-    }
-  }
+  // for (let x = 0; x < 25; ++x) {
+  //   for (let y = 13; y < 14; ++y) {
+  //     sprites.drawTile("ground", context, x, y);
+  //   }
+  // }
 });
