64caaeaa1a7fd88900af4b5ed933d764ca499524
Sean Stoneburner <seanstoneburner@Seans-MacBook-Pro.local>
2020-04-18 11:13:18 -0400

    using promise.all() to load background sprites and to draw background background, both in parallel

--- public/js/main.js
+++ public/js/main.js
@@ -1,7 +1,7 @@
 import SpriteSheet from "./SpriteSheet.js";
 import { loadImage, loadLevel } from "./loaders.js";
 
-function drawBackground(background, context, sprites) {
+const drawBackground = (background, context, sprites) => {
   background.ranges.forEach(([x1, x2, y1, y2]) => {
     for (let x = x1; x < x2; ++x) {
       for (let y = y1; y < y2; ++y) {
@@ -9,32 +9,25 @@ function drawBackground(background, context, sprites) {
       }
     }
   });
-}
+};
+
+const loadBackgroundSprites = () => {
+  return loadImage("/images/tiles.png").then((image) => {
+    const sprites = new SpriteSheet(image, 16, 16);
+    sprites.define("sky", 1, 0);
+    sprites.define("grass", 0, 0);
+    sprites.define("ground", 0, 1);
+    return sprites;
+  });
+};
 
 const canvas = document.getElementById("screen");
 const context = canvas.getContext("2d");
 
-loadImage("/images/tiles.png").then((image) => {
-  const sprites = new SpriteSheet(image, 16, 16);
-  sprites.define("sky", 1, 0);
-  sprites.define("grass", 0, 0);
-  sprites.define("ground", 0, 1);
-
-  loadLevel("1-1").then((level) => {
+Promise.all([loadBackgroundSprites(), loadLevel("1-1")]).then(
+  ([sprites, level]) => {
     level.backgrounds.forEach((background) =>
       drawBackground(background, context, sprites)
     );
-  });
-
-  // for (let x = 0; x < 25; ++x) {
-  //   for (let y = 12; y < 13; ++y) {
-  //     sprites.drawTile("grass", context, x, y);
-  //   }
-  // }
-
-  // for (let x = 0; x < 25; ++x) {
-  //   for (let y = 13; y < 14; ++y) {
-  //     sprites.drawTile("ground", context, x, y);
-  //   }
-  // }
-});
+  }
+);
